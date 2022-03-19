015900aeafa5a400baf28425892ff38484f68c77
Win-10 <Win-10@DESKTOP-VH1GHO7>
2022-03-19 21:49:17 +0530

    New Commit

--- C2TC_Project/src/FirstProgram.java
+++ C2TC_Project/src/FirstProgram.java
@@ -3,7 +3,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello\ncore Java");
+		System.out.println("Hello\nCore Java");
 
 	}
 
