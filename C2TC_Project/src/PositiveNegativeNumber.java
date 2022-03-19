849cc33e0eb00d94f02b05c00c82ebb2e32ef134
Win-10 <Win-10@DESKTOP-VH1GHO7>
2022-03-19 22:03:51 +0530

    New Commit

---
+++ C2TC_Project/src/PositiveNegativeNumber.java
@@ -0,0 +1,21 @@
+
+public class PositiveNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=912;   
+		if(num>0)  
+		{  
+		System.out.println("The number is positive.");  
+		}  
+		else if(num<0)  
+		{  
+		System.out.println("The number is negative.");  
+		}    
+		else  
+		{  
+		System.out.println("The number is zero.");  
+
+	}
+	}
+}
