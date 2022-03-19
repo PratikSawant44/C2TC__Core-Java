d683a5245d82733f49ebdc7581ddc8150f6c9058
Win-10 <Win-10@DESKTOP-VH1GHO7>
2022-03-19 20:37:12 +0530

    New Commit

---
+++ C2TC_Project/src/QuotientRemainder.java
@@ -0,0 +1,14 @@
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 15, num2 = 2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+
+	}
+
+}
