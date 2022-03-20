a6e6251852a2855047347402fe80975a6d67d89e
Win-10 <Win-10@DESKTOP-VH1GHO7>
2022-03-20 23:37:40 +0530

    New Commit

---
+++ C2TC_Project/src/Base.java
@@ -0,0 +1,19 @@
+//super keyword
+class Parent {
+ 
+    int a = 10;
+    static int b = 20;
+}
+public class Base extends Parent {
+	void rr()
+    {
+		System.out.println(super.a);
+		System.out.println(super.b);
+    }
+	public static void main(String[] args)
+    {
+		new Base().rr();
+    }
+
+
+}
